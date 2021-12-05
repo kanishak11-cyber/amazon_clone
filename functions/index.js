5216a19acda6d94590ca06af42227241fedb96d4
KANISHAK CHAURASIA <kanishakchaurasia2@gmail.com>
2021-12-05 15:19:30 +0530

    done almost

--- functions/index.js
+++ functions/index.js
@@ -22,8 +22,7 @@ app.post("/payments/create", async (request, response) => {
     response.status(201).send({
         clientSecret: paymentIntent.client_secret,
     });
-})
-
+});
 
 // http://localhost:5001/clone-3d278/us-central1/api
 exports.api = functions.https.onRequest(app);
